2a6aad953e95053e1130d9faedca6f2337e26cef
arthurzucker <arthur.zucker@gmail.com>
2021-11-17 08:10:23 +0100

    base loader updated

--- datasets/base_dataloader.py
+++ datasets/base_dataloader.py
@@ -33,6 +33,5 @@ class base_dataloader():
         self.train_iterations = (self.len_train_data + self.config.batch_size - 1) // self.config.batch_size
         self.valid_iterations = (self.len_valid_data + self.config.batch_size - 1) // self.config.batch_size
 
-
-        self.train_loader = DataLoader(train_dataset, batch_size=self.config.batch_size, shuffle=True)
-        self.valid_loader = DataLoader(valid_dataset, batch_size=self.config.batch_size, shuffle=False) 
+        self.train_loader = DataLoader(train_dataset, batch_size=self.config.batch_size, shuffle=True,num_workers=self.config.num_workers)
+        self.valid_loader = DataLoader(valid_dataset, batch_size=self.config.batch_size, shuffle=False,num_workers=self.config.num_workers) 
